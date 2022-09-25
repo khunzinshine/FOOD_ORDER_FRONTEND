52ce6ecc7bd127f7174792ca4177780167bf6e81
khunzinshine <khunzinshine@gmail.com>
2022-09-25 16:28:47 +0630

    modify header tttle

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,17 @@
-import React, { Fragment } from "react";
-import Meal from "../../assets/meals.jpg";
-import classes from "./Header.module.css";
-import HeaderCartButton from "./HeaderCartButton";
+import React, { Fragment } from 'react';
+import Meal from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>90's House</h1>
+        <h1>1998</h1>
         <HeaderCartButton onClick={props.showModal} />
       </header>
-      <div className={classes["main-image"]}>
-        <img src={Meal} alt="A table full of delicious food!" />
+      <div className={classes['main-image']}>
+        <img src={Meal} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
